869eecd5eb92167af0bf0cf21c3d0a9ac550c865
Daiyan Rafi <daiyanrafi1@gmail.com>
2022-03-22 01:47:49 +0600

    files added

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static final Color primaryColor = Color.fromRGBO(128, 128, 128, 1);
+  static final Color scaffoldBackgroundColor = Color.fromRGBO(245, 247, 249, 1);
+}
